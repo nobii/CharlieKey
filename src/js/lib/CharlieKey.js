abb00cd2a57ff2974204a3ecb93afc9c683bf6b4
fujisawa-shin <fujisawa-shin@kayac.com>
2015-11-24 12:42:58 +0900

    test書いた。

---
+++ src/js/lib/CharlieKey.js
@@ -0,0 +1,23 @@
+var CharlieKey = function (opts) {
+    opts = opts || {};
+    this._keyCode = undefined;
+};
+
+CharlieKey.prototype.registerKeyCode = function (keyCode) {
+    if (!isNaN(this._keyCode)) {
+        return;
+    }
+    if (!isNaN(keyCode) && 0 <= keyCode && keyCode <= 9999) {
+        this._keyCode = keyCode;
+    }
+};
+
+CharlieKey.prototype.inputKeyCode = function (keyCode) {
+    if (this._keyCode == keyCode) {
+        this._keyCode = undefined;
+    }
+};
+
+CharlieKey.prototype.isOpened = function () {
+    return isNaN(this._keyCode);
+};
